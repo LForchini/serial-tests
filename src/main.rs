bdbaa402d8cbfed0cf7e8b91dfe01363b3000d02
LForchini <git@lforchini.com>
2024-02-21 10:49:18 +0000

    some updates to use tokio::io::split

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-mod serial_stream;
+use std::{env, time::Duration};
 
-use std::env;
-
-use serial_stream::SerialStreamExt;
-use tokio::io::{stdin, AsyncBufReadExt, BufReader};
+use std::fmt::Debug;
+use tokio::io::{stdin, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter, Lines};
 use tokio_serial::SerialPortBuilderExt;
+use tracing::{info, instrument, trace, warn};
+use tracing_subscriber::{layer::SubscriberExt, Registry};
+use tracing_tree::HierarchicalLayer;
 
+#[instrument]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let ports = tokio_serial::available_ports().expect("No ports found!");
+    let layer = HierarchicalLayer::default()
+        .with_writer(std::io::stderr)
+        .with_indent_lines(true)
+        .with_indent_amount(2)
+        .with_thread_names(true)
+        .with_thread_ids(true)
+        .with_verbose_exit(false)
+        .with_verbose_entry(true)
+        .with_targets(true);
+
+    let subscriber = Registry::default().with(layer);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let ports = tokio_serial::available_ports().expect("Ports should be readable");
 
     if ports.len() == 0 {
         panic!("no serial ports found")
@@ -16,39 +31,48 @@ async fn main() -> anyhow::Result<()> {
 
     let port_name = &env::args().collect::<Vec<_>>()[1];
 
-    let (tx, mut rx, k, jh0) = tokio_serial::new(port_name, 9600)
-        .open_native_async()
-        .unwrap()
-        .split();
+    let (read, write) = tokio::io::split(
+        tokio_serial::new(port_name, 9600)
+            .open_native_async()
+            .unwrap(),
+    );
 
-    let _jh1 = tokio::spawn(async move {
-        let stdin = stdin();
+    let mut br = BufReader::new(read).lines();
+    let mut bw = BufWriter::new(write);
 
-        let mut lines = BufReader::new(stdin).lines();
+    let mut stdin = BufReader::new(stdin()).lines();
 
-        loop {
-            if let Ok(Some(line)) = lines.next_line().await {
-                tx.send((line + "\n").as_bytes().into()).await.unwrap();
-            } else {
-                k.send(()).unwrap();
-                break;
+    info!("entering ready state");
+    loop {
+        tokio::select! {
+            Ok(Some(line)) = br.next_line() => {
+                handle_read(line).await;
             }
-        }
-    });
 
-    tokio::spawn(async move {
-        loop {
-            if let Some(line) = rx.recv().await {
-                if let Ok(line) = std::str::from_utf8(&line) {
-                    print!("{}", line);
-                } else {
-                    eprintln!("got malformed packet");
+            Ok(opt_line) = stdin.next_line() => {
+                match opt_line {
+                    Some(line) => {handle_write(&mut bw, line).await;},
+                    None => {break;}
                 }
             }
         }
-    });
+    }
 
-    jh0.await??;
+    bw.flush().await.unwrap();
+    trace!(?bw, "flushed");
 
     Ok(())
+}
+
+#[instrument]
+async fn handle_write<T>(bw: &mut BufWriter<T>, line: String)
+where
+    T: Debug + AsyncWrite + AsyncWriteExt + Unpin,
+{
+    bw.write_all((line + "\n").as_bytes()).await.unwrap();
+}
+
+#[instrument]
+async fn handle_read(line: String) {
+    println!("{line}");
 }
